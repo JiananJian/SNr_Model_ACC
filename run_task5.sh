f27262463ec57e21267fad0f7562c7d62554b7f2
jianan jian <JiananJian@users.noreply.github.com>
2024-09-22 16:37:28 -0400

    Add files via upload

---
+++ run_task5.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+TASK_NAME=task5v2
+
+module load cuda/11.7.1
+
+echo "#!/bin/bash" > "${TASK_NAME}.job"
+echo "#SBATCH -N 1" >> "${TASK_NAME}.job"
+echo "#SBATCH -p GPU-shared" >> "${TASK_NAME}.job"
+echo "#SBATCH --gpus=v100:1" >> "${TASK_NAME}.job"
+echo "#SBATCH -t 60" >> "${TASK_NAME}.job"
+echo "#SBATCH -A mth240008p" >> "${TASK_NAME}.job"
+echo "./${TASK_NAME}.out" >> "${TASK_NAME}.job" $@
+
+sbatch "${TASK_NAME}.job"
+squeue -u jjian
